a4647e102ab644431ad8881a0987ec44ae529a1b
ssar <skshahra@gmail.com>
2022-08-29 22:34:17 +0600

    29aug22

--- src/routes/s/create/index.svelte
+++ src/routes/s/create/index.svelte
@@ -4,6 +4,7 @@
 <script lang="ts">
 	import Camera from '$lib/icons/camera.svelte';
 	import Errorsign from '$lib/icons/Errorsign.svelte';
+import RoundPlus from '$lib/icons/roundPlus.svelte';
 
 	let avatar: any;
 	let FileInput: any;
@@ -34,15 +35,55 @@
 
 	let ErrorMsg = {
 		Name: [true, "User Name is't valid"],
-		UserId: [true, 'Password is too short']
+		UserId: [true, 'User Id is not Available']
 	};
 
 	function OnSubmit() {}
+
+	let AdminObj={
+		input: '' as string,
+		focus: false as boolean,
+		list: [] as any,
+		suggest: [] as any,
+	};
+	// AdminObj.input = '';
+	// AdminObj.focus = false;
+
+	AdminObj.list = [
+		{ name: 'sk shahriar ahmed raka', UserId: 'skrajhkka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' },
+		{ name: 'sk  ahmed raka', UserId: 'skghjkhraka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' },
+		{ name: 'ahmed raka', UserId: 'skrajghka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' },
+		{ name: 'sk shahriar  raka', UserId: 'shjkghkraka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' }
+	];
+	AdminObj.suggest = [
+		{ name: 'what sk shahriar ahmed ', UserId: 'svghjkraka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' },
+		{ name: 'why shahriar ahmed raka', UserId: 'skhgvraka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' },
+		{ name: 'how shahriar ahmed raka', UserId: 'skhgjraka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' },
+		{ name: 'never shahriar ahmed raka', UserId: 'shgvjkraka', ProfileImage: 'https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png' }
+	];
+	console.log("🚀 ~ file: index.svelte ~ line 43 ~ AdminObj", AdminObj)
+
+	function onChangeAdminObj(e: { keyCode: any }) {
+		switch (e.keyCode) {
+			case 32:
+			case 13:
+				onKeyTag();
+				break;
+		}
+	}
+	function onKeyTag() {
+		if (AdminObj.input.trim() != ('' as String)) {
+			AdminObj.list = [...AdminObj.list, AdminObj.input.trim()];
+			AdminObj.input = '' as string;
+		}
+	}
 </script>
 
-<div class=" flex  h-screen w-full flex-row overflow-hidden bg-[#36393f] ">
-	<div class=" h-full w-1/2 justify-center overflow-y-scroll ">
-		<div class="flex flex-col w-full ">
+<div
+	class=" flex  h-screen w-full overflow-hidden bg-[#36393f] md:flex-col md:overflow-y-scroll lg:flex-row "
+>
+	<div class=" h-fit justify-center  md:w-full lg:w-1/2 lg:overflow-y-scroll ">
+		<div class="flex w-full flex-col ">
 			<!-- save & discard button -->
 			<div class=" absolute right-0">
 				<button
@@ -52,27 +93,27 @@
 				</button>
 
 				<button
-					class=" m-4 h-12 w-40 self-center rounded-xl bg-transparent border-[1px] border-red-500 text-xl font-bold text-red-500 hover:bg-red-300 hover:bg-opacity-50 active:bg-opacity-30 active:bg-red-200"
+					class=" m-4 h-12 w-40 self-center rounded-xl border-[1px] border-red-500 bg-transparent text-xl font-bold text-red-500 hover:bg-red-300 hover:bg-opacity-50 active:bg-red-200 active:bg-opacity-30"
 				>
 					discard
 				</button>
 			</div>
 			<!-- banner -->
-			<div class="flex flex-row  justify-center">
+			<div class="flex flex-row  justify-center ">
 				{#if avatar}
 					<img
 						src={avatar}
 						alt="ProfileImage"
-						class=" aspect-video mt-6 w-[80%] self-center rounded-lg object-cover"
+						class=" mt-6 aspect-[16/9] w-[80%] self-center rounded-lg object-cover"
 					/>
 				{:else}
 					<img
 						src="https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png"
 						alt="ProfileImage"
-						class=" aspect-video mt-6 w-[80%] self-center rounded-lg object-cover "
+						class=" mt-6 aspect-[16/9] w-[80%]  self-center rounded-lg object-cover "
 					/>
 				{/if}
-				<button class=" -mt-20 -ml-16 self-end hover:contents  " on:click={() => FileInput.click()}>
+				<button class=" absolute   self-end   " on:click={() => FileInput.click()}>
 					<!-- <img src="https://static.thenounproject.com/png/625182-200.png" alt="uploadImage" class=" h-28 w-28"> -->
 					<Camera class="h-10 w-10 stroke-slate-100 stroke-1 opacity-50  hover:opacity-100" />
 					<input
@@ -85,28 +126,28 @@
 				</button>
 			</div>
 			<!-- profile image , name , id -->
-			<div class=" flex flex-row w-full  ">
+			<div class=" flex w-full flex-row  justify-center  ">
 				<!-- profile image -->
-				<div class="  mt-6 flex w-1/3   justify-center">
+				<div class="  relative mt-6 flex  w-1/4  justify-center">
 					{#if UserAvatar}
 						<img
 							src={UserAvatar}
 							alt="ProfileImage"
-							class=" aspect-square mt-6 w-[40%] self-center rounded-lg object-cover"
+							class=" aspect-[1/1]   w-[60%] self-center rounded-lg object-cover "
 						/>
 					{:else}
 						<img
 							src="https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png"
 							alt="ProfileImage"
-							class=" aspect-[1/1]   w-[60%] self-center rounded-lg object-cover  "
+							class=" aspect-[1/1] w-[80%] self-center rounded-lg object-cover  "
 						/>
 					{/if}
-					<button class=" -mt-0 -ml-10  self-end   " on:click={() => FileInput.click()}>
+					<button class=" absolute  self-end   " on:click={() => UserFileInput.click()}>
 						<!-- <img src="https://static.thenounproject.com/png/625182-200.png" alt="uploadImage" class=" h-28 w-28"> -->
 						<Camera class="h-10 w-10 stroke-slate-100 stroke-1 opacity-50  hover:opacity-100" />
 						<input
 							bind:this={UserFileInput}
-							on:change={(e) => onFileSelected(e)}
+							on:change={(e) => onFileSelectedUser(e)}
 							type="file"
 							class=" hidden h-10 w-10 "
 							accept=".jpg, .jpeg, .png, .svg"
@@ -114,11 +155,11 @@
 					</button>
 				</div>
 				<!-- name , id -->
-				<div class=" mt-6  self-start w-2/3 ">
+				<div class=" mt-6  w-2/4 ">
 					<!--  -->
-					<!-- email or  mobile -->
+					<!-- Name  -->
 					<div class=" flex flex-row self-start text-left text-sm font-semibold text-gray-300 ">
-						<div class=" ml-12 mt-2 self-start text-left text-base font-semibold text-gray-300 ">
+						<div class=" ml-6 mt-2 self-start text-left text-base font-semibold text-gray-300 ">
 							Name :
 						</div>
 						{#if ErrorMsg.Name[0]}
@@ -134,9 +175,9 @@
 						type="text"
 						class=" mx-4 my-2 h-12 w-[415px] self-center rounded-2xl border-2 border-[#24262b] bg-[#303338]  p-2 text-lg font-medium text-[#98999e] outline-none  focus:border-sky-500 active:border-gray-800 "
 					/>
-					<!-- email or  mobile -->
+					<!-- User id -->
 					<div class=" flex flex-row self-start text-left text-sm font-semibold text-gray-300 ">
-						<div class=" ml-12 mt-2 self-start text-left text-base font-semibold text-gray-300 ">
+						<div class=" ml-6 mt-2 self-start text-left text-base font-semibold text-gray-300 ">
 							User Id :
 						</div>
 						{#if ErrorMsg.UserId[0]}
@@ -156,9 +197,65 @@
 				</div>
 			</div>
 		</div>
+		<!-- admins -->
+		<div class="flex flex-col justify-center  object-center ">
+			<p class="self-center text-left  text-xl font-semibold text-gray-400">Admins:</p>
+			<div class=" flex flex-row flex-wrap w-fit gap-2 self-center">
+				{#each AdminObj.list as t}
+					<li
+						class=" bg-slate-600 rounded-lg h-10 w-fit flex flex-row "
+					>
+					<!-- class="m-1 rounded-md bg-[#3d4951] px-2 py-1 text-[#9bc0da] hover:cursor-pointer hover:bg-slate-600 hover:text-teal-200" -->
+						<!-- {t} -->
+						<img src={t.ProfileImage} alt="" class=" w-6 h-6 object-cover rounded-lg">
+						<div class=" flex flex-col">
+							<p class=" text-sm">{t.name}</p>
+							<p class=" text-xs">{t.UserId}</p>
+						</div>
+					</li>
+				{/each}
+			</div>
+			<!-- input admin name and suggession -->
+			<div class="flex w-[65%] flex-col gap-1 self-center">
+				<div class=" flex h-16 w-full flex-row">
+					<RoundPlus/>
+					<input
+						on:keypress={onChangeAdminObj}
+						maxlength="20"
+						bind:value={AdminObj.input}
+						on:focus={() => {
+							AdminObj.focus = true;
+						}}
+						on:blur={() => {
+							AdminObj.focus = false;
+						}}
+						type="text"
+						class=" mx-2 mt-3 inline h-fit w-full  grow rounded-lg  border-2   bg-[#45494e] bg-inherit p-2  text-lg  font-medium text-[#98999e]  outline-none outline-0 focus:border-2 focus:shadow-none  focus:border-[#3ba55d] focus:outline-none active:border-none  active:outline-none "
+					/>
+				</div>
+				<!-- render seggestions -->
+				{#each AdminObj.suggest as i}
+					<div
+						class="ml-5 flex h-16 w-[85%] cursor-pointer   flex-row items-center gap-3 self-center rounded-lg bg-slate-600 pl-3 hover:bg-gray-500 active:bg-gray-400"
+					>
+						<img
+							src="https://res.cloudinary.com/dqo0ssnti/image/upload/v1661613541/samples/Untitled_design_zmrybr.png"
+							alt="ProfileImage"
+							class=" h-12 w-12 rounded-lg object-cover"
+						/>
+						<div class="flex flex-col">
+							<p class="">{i.name}</p>
+							<p class="">@{i.UserId}</p>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</div>
 	</div>
 	<!-- middle bar -->
-	<div class=" h-[80%] w-[1px] self-center bg-white" />
+	<div class=" h-[80%] w-[1px] self-center bg-slate-500 md:hidden lg:block" />
 
-	<div class=" h-full w-1/2 overflow-y-scroll" />
+	<div class=" flex h-fit flex-col md:w-full lg:w-1/2 lg:overflow-y-scroll ">
+		<!--  -->
+	</div>
 </div>
